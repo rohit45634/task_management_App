8c48fa4addd1c96dcf2cbc26cb13353b2a7f7d19
rohit <rohitmaurya2674463@gmail.com>
2026-06-12 14:20:16 +0530

    change = add

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -6,7 +6,7 @@ import genToken from "../config/token.js";
 export const register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    if (password.length < 8) {
+    if (password.length <= 8) {
       return res.status(400).json({
         message: "Password must be at least 8 characters",
       });
@@ -56,7 +56,7 @@ export const login = async (req, res) => {
         message: "Wrong Password",
       });
     }
-    if (compare.length < 8) {
+    if (compare.length <= 8) {
       return "Password must be at least 8 characters";
     }
 
